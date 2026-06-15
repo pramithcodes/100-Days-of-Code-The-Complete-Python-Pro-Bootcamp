35472122359b68b3b211cad0e70f530adcaff005
Pramith N <pramith.codes@gmail.com>
2026-06-16 03:25:10 +0530

    Create Day 01 - Band Name Generator.py
    Update: Created a Band Name Generator using basic Python Programming.

---
+++ Day 01 - Band Name Generator.py
@@ -0,0 +1,7 @@
+print("Hello, Welcome to the Band Name Generator!")
+
+name_1 = input("What is your stage name?: ")
+name_2 = input("What funky name would you like to keep?: ")
+
+# Adding both strings using string concatenation and shifting the sentence to a new line using \n
+print("Your band name would be: " + name_1 + " " + name_2 + "!\nThank you for using the Band Name Generator.")
